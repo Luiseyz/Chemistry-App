9a345cd300abd8735b6774b51c9a99dc79b022cc
Luiseyz <133915115+Luiseyz@users.noreply.github.com>
2023-05-22 13:51:37 -0600

    Add files via upload

---
+++ Final Chemistry AppUITests/Final_Chemistry_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Final_Chemistry_AppUITestsLaunchTests.swift
+//  Final Chemistry AppUITests
+//
+//  Created by Luis Armando Eyzaguirre on 26/04/23.
+//
+
+import XCTest
+
+final class Final_Chemistry_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
